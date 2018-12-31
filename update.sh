1a5fc8207d8ea6b3604026b30d227f6be53c5eb9
Choe, Cheng-Dae <whitekid@gmail.com>
2018-12-31 10:18:55 +0900

    fix update gh-pages

--- update.sh
+++ update.sh
@@ -2,10 +2,9 @@
 make build
 git checkout -f gh-pages
 git checkout master -- feeds.ini
-bin/goplanet > index.xml
-git diff index.xml
+bin/goplanet
 git rm -f feeds.ini
-git add index.xml feeds.ini
+git add golang.xml
 git commit --amend -C HEAD
 git push -f
 git checkout -f master
